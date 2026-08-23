c66b204ed035443ac4374547bf2a6169022180c5
Ghosty-sudo <113943247+Ghosty-sudo@users.noreply.github.com>
2026-08-23 18:22:33 -0400

    Fix GREED PIT cache updates for 0.20.2

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE='greed-pit-0.10-browser';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match('./index.html'))));
+const CACHE='greed-pit-0.20.2';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{
+});
+self.addEventListener('activate',event=>{
+});
+self.addEventListener('fetch',event=>{
 });
