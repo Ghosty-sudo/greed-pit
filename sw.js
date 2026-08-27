682b1625a7ada1f25e0aa9f986079bcbe5bdf373
Ghosty-sudo <113943247+Ghosty-sudo@users.noreply.github.com>
2026-08-27 19:52:21 -0400

    Update GREED PIT service worker cache to 0.20.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='greed-pit-0.20.4';
+const CACHE='greed-pit-0.20.5';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   event.waitUntil(
